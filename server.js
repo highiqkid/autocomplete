f97876d5311a7c89ed2ea74b4ac2046fa9a35552
highiqkid <victor_cheng@yahoo.com>
2016-05-27 20:13:11 -0700

    server added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(process.env.PORT, 'localhost', function (err, result) {
+}).listen(process.env.PORT, function (err, result) {
   if (err) {
 	  
     console.log(err);
